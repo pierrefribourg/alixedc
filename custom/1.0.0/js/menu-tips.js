97d4d7fb9ad21ce8c08e98929e57460bb5c8ebe5
ThomasPerraudin <github@perraudin.fr>
2012-12-04 18:29:22 +0100

    Menu tips modified (adminsitration)

--- custom/1.0.0/js/menu-tips.js
+++ custom/1.0.0/js/menu-tips.js
@@ -12,7 +12,7 @@ var menuTips = {
   queries:    'Consult and manage queries : suggest corrections, close resolved queries, open new ones.',
   deviations: 'Consult and manage protocol deviations.',
   audittrail: 'Explore the history of data.',
-  tools:      'Acces to the administration board and manage:<ul><li>Users : create users and manage their rights</li><li>Sites : create and manage sites</li><li>ClinicalData : consult the list of subjects\' documents (XML CDISC)</li><li>MetaData : Access to the study design\'s document (XML CDISC)</li><li>Import Metadata/ClinicalData : import and replace documents (XML CDISC)</li><li>Import Clinical Data</li><li>Data export : export data for statistical purposes</li><li>Editor : edit CDSIC documents and modify specific scripts (user interface, behavior of pages)</li></ul>',
+  tools:      'Access to the administration board',// and manage:<ul><li>Users : create users and manage their rights</li><li>Sites : create and manage sites</li><li>ClinicalData : consult the list of subjects\' documents (XML CDISC)</li><li>MetaData : Access to the study design\'s document (XML CDISC)</li><li>Import Metadata/ClinicalData : import and replace documents (XML CDISC)</li><li>Import Clinical Data</li><li>Data export : export data for statistical purposes</li><li>Editor : edit CDSIC documents and modify specific scripts (user interface, behavior of pages)</li></ul>',
   logout:     'Close your session.'
 };
 
