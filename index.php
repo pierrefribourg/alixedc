e93b077c4ab97c12f4cbdaa4c799ac2315170d3c
ThomasPerraudin <github@perraudin.fr>
2013-01-28 11:30:50 +0100

    x modifis

--- index.php
+++ index.php
@@ -21,4 +21,4 @@
     \**************************************************************************/
 
 require_once("config.inc.php");
-header("Location: ../index.php?menuaction=alixedc.uietude.dashboardInterface");	
+header("Location: ../index.php?menuaction=".$moduleName.".uietude.startupInterface");	
